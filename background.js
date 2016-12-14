82fc2d7b0b3d9b6cf81d4834f73e8cd1b2c84201
Twixt0r <omer_cetin74@hotmail.de>
2016-12-14 20:30:41 +0100

    aranged

--- background.js
+++ background.js
@@ -1,4 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(activeTab)
 {   
-    var RedditIT = chrome.tabs.query({currentWindow: true, active: true}, function(tabs) { chrome.tabs.create({url: "https://www.reddit.com/submit?title=" + tabs[0].title + "&url=" + tabs[0].url}); });
+    chrome.tabs.query({currentWindow: true, active: true}, 
+                      function(tabs) { 
+        chrome.tabs.create({url: "https://www.reddit.com/submit?title=" + tabs[0].title + "&url=" + tabs[0].url}); });
 });
